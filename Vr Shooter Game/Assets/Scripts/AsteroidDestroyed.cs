99c52e40ef88fc65efb81e244d13846fe20bf95e
Abhyjit <rrabhyjit0@gmail.com>
2023-06-12 15:53:18 +0530

    game basics completed

--- Vr Shooter Game/Assets/Scripts/AsteroidDestroyed.cs
+++ Vr Shooter Game/Assets/Scripts/AsteroidDestroyed.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using TMPro;
 
 public class AsteroidDestroyed : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class AsteroidDestroyed : MonoBehaviour
     [SerializeField] private Pose player;
 
     [SerializeField] private GameController gameController;
+    [SerializeField] private GameObject popupcanvas;
 
 
     public void Awake()
@@ -16,19 +19,31 @@ public class AsteroidDestroyed : MonoBehaviour
     }
     public void AsteroidExplosion()
     {
-        Instantiate(astroidExplosion, transform.position,transform.rotation);   //instantiate is used to add objects in to the scene and here it is used to istantiate destroying asteroid
-        
+        Instantiate(astroidExplosion, transform.position, transform.rotation);   //instantiate is used to add objects in to the scene and here it is used to istantiate destroying asteroid
+
         //calculate the score 
-        float distanceFromPlayer = Vector3.Distance(transform.position,player.position);
+        float distanceFromPlayer = Vector3.Distance(transform.position, player.position);
         int bonusPoints = (int)distanceFromPlayer;
 
         int asteroidScore = 10 * bonusPoints;
 
         // passing asteroid score value to the game controller script
         gameController.UpdatePlayerScore(asteroidScore);
-        
-        
-        
+
+
+
+        // instantiate popup canvas in asteroids
+
+       popupcanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = asteroidScore.ToString();
+        GameObject asteroidpopup=Instantiate(popupcanvas, transform.position, quaternion.identity); // this is to instantiate a canvas with score*/
+
+
+        //adjust the scale of the popup
+        asteroidpopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 5),
+                                                          transform.localScale.y * (distanceFromPlayer / 5),
+                                                          transform.localScale.z * (distanceFromPlayer / 5));
+
+
         Destroy(this.gameObject);//since this is added to the asteroid istself .....after instantiating destroying object destroying this object is called which will destroy the asteroid itself
     }
 
