8f2ff9e03ec04920b565a5616fe6625ec97922b3
Abhyjit <rrabhyjit0@gmail.com>
2023-06-15 11:24:24 +0530

    start menu created

--- Vr Shooter Game/Assets/Scripts/AsteroidDestroyed.cs
+++ Vr Shooter Game/Assets/Scripts/AsteroidDestroyed.cs
@@ -20,28 +20,31 @@ public class AsteroidDestroyed : MonoBehaviour
     public void AsteroidExplosion()
     {
         Instantiate(astroidExplosion, transform.position, transform.rotation);   //instantiate is used to add objects in to the scene and here it is used to istantiate destroying asteroid
+        if(GameController.CurrentGameStatus == GameController.GameState.Playing) 
+        {
+            //calculate the score 
+            float distanceFromPlayer = Vector3.Distance(transform.position, player.position);
+            int bonusPoints = (int)distanceFromPlayer;
 
-        //calculate the score 
-        float distanceFromPlayer = Vector3.Distance(transform.position, player.position);
-        int bonusPoints = (int)distanceFromPlayer;
+            int asteroidScore = 10 * bonusPoints;
 
-        int asteroidScore = 10 * bonusPoints;
-
-        // passing asteroid score value to the game controller script
-        gameController.UpdatePlayerScore(asteroidScore);
-
-
-
-        // instantiate popup canvas in asteroids
-
-       popupcanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = asteroidScore.ToString();
-        GameObject asteroidpopup=Instantiate(popupcanvas, transform.position, quaternion.identity); // this is to instantiate a canvas with score*/
+            // passing asteroid score value to the game controller script
+            gameController.UpdatePlayerScore(asteroidScore);
 
 
-        //adjust the scale of the popup
-        asteroidpopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 5),
-                                                          transform.localScale.y * (distanceFromPlayer / 5),
-                                                          transform.localScale.z * (distanceFromPlayer / 5));
+
+            // instantiate popup canvas in asteroids
+
+            popupcanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = asteroidScore.ToString();
+            GameObject asteroidpopup = Instantiate(popupcanvas, transform.position, quaternion.identity); // this is to instantiate a canvas with score*/
+
+
+            //adjust the scale of the popup
+            asteroidpopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 5),
+                                                              transform.localScale.y * (distanceFromPlayer / 5),
+                                                              transform.localScale.z * (distanceFromPlayer / 5));
+        }
+
 
 
         Destroy(this.gameObject);//since this is added to the asteroid istself .....after instantiating destroying object destroying this object is called which will destroy the asteroid itself
--- Vr Shooter Game/Assets/Scripts/GameController.cs
+++ Vr Shooter Game/Assets/Scripts/GameController.cs
@@ -13,8 +13,26 @@ public class GameController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI score;
 
     private int PlayerScore;
+
+
+    public enum GameState
+    {
+        Waiting,
+        Playing,
+        GameOver
+    }
+    public static GameState CurrentGameStatus;
+
+
+
+
+    private void Awake()
+    {
+        CurrentGameStatus = GameState.Waiting;
+    }
     void Update()
     {
+        if (CurrentGameStatus == GameState.Playing)
         Timer();
         
     }
@@ -28,7 +46,12 @@ public class GameController : MonoBehaviour
     }
     public void UpdatePlayerScore(int AsteroidHitScore)
     {
+        if(CurrentGameStatus != GameState.Playing) { return; }
         PlayerScore += AsteroidHitScore;
         score.text = PlayerScore.ToString();
+    }
+    public void StartGame()
+    {
+        CurrentGameStatus = GameState.Playing;
     }
 }
